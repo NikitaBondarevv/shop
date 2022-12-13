c2612ee5bc117c4f766f09adb240380855ad48ac
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2022-12-13 14:17:46 +0200

    Code refactoring

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,24 @@
 import { NumberOfProductsAndCategories } from '../numberOfProductsAndCategories'
-import { products } from '../helpers/products'
 import { ProductInformation } from '../productInformation'
+import { getCategories } from '../contracts/getCategories'
 import styles from './styles.css'
+import { useEffect, useState } from 'react'
 
-export const Main = () => (
-  <main className={styles.main}>
-    <NumberOfProductsAndCategories name="Nikita" products={products} />
-    <ProductInformation />
-  </main>
-)
+export const Main = () => {
+  const [products, setProducts] = useState({})
+
+  useEffect(() => {
+    const getData = async () => {
+      setProducts(await getCategories())
+    }
+
+    getData()
+  }, [])
+
+  return (
+    <main className={styles.main}>
+      <NumberOfProductsAndCategories name="Nikita" products={products} />
+      <ProductInformation />
+    </main>
+  )
+}
--- src/numberOfProductsAndCategories/index.js
+++ src/numberOfProductsAndCategories/index.js
@@ -1,24 +1,6 @@
 import styles from './styles.css'
 
 export const NumberOfProductsAndCategories = ({ name, products }) => {
-  const productsAndCategories = products
-
-  const getNumberOfCategories = () => productsAndCategories.map(product => product.category)
-
-  const getNumberOfProducts = () => productsAndCategories.map(product => product.product)
-
-  const getNumberOfPublished = () => {
-    const published = []
-
-    productsAndCategories.map(product => {
-      if (product.published === true) {
-        published.push(product.published)
-      }
-    })
-
-    return published
-  }
-
   return (
     <div className={styles.productsInfo}>
       <span className={styles.greetings}>
@@ -27,11 +9,11 @@ export const NumberOfProductsAndCategories = ({ name, products }) => {
       </span>
       <ul className={styles.list}>
         <li>
-          You have {getNumberOfCategories().length} categories
-          ({getNumberOfPublished().length} published)
+          You have {products.categories} categories
+          ({products.publishedCategories} published)
         </li>
         <li>
-          You have {getNumberOfProducts().length} products
+          You have {products.products} products
         </li>
       </ul>
       <a className={styles.categories} href="#">Go to categories</a>
