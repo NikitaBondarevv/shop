11fb28ccdd84cfc9252b517f9245d7f8a7df54af
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2022-12-16 17:24:12 +0200

    Code refactoring

--- src/productInformation/index.js
+++ src/productInformation/index.js
@@ -2,7 +2,9 @@ import { EditableText } from './editableText'
 
 import styles from './styles.css'
 
-const descriptionText = 'Enjoy new movie-themed thrills ad a favorite Radiator Springs location - Willys Butte! Inspired by Disney Pixar Cars 3, this hair-raising set uses multiple wild driving skills and transforms for 3 ways play.'
+const descriptionText = `Enjoy new movie-themed thrills ad a favorite Radiator Springs
+location - Willys Butte! Inspired by Disney Pixar Cars 3,
+this hair-raising set uses multiple wild driving skills and transforms for 3 ways play.`
 
 export const ProductInformation = () => (
   <div className={styles.productInformation}>
@@ -11,7 +13,8 @@ export const ProductInformation = () => (
       <EditableText text="TOW TRACK" stylesInput={styles.editableTitle} />
     </span>
     <span className={styles.price}>
-      $ <EditableText text="32" stylesInput={styles.editablePrice} />
+      $
+      <EditableText text="32" stylesInput={styles.editablePrice} />
     </span>
     <div className={styles.description}>
       <EditableText text={descriptionText} multiLine />
