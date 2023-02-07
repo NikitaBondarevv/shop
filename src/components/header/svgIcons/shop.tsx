4ef8ee1e11bd069cd03b610c02f706735dfbbbe3
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2023-02-07 20:19:36 +0200

    Code refactoring in shop.tsx

--- src/components/header/svgIcons/shop.tsx
+++ src/components/header/svgIcons/shop.tsx
@@ -2,7 +2,7 @@ import styles from '../styles.css'
 
 export const ShopIcon = () => (
   <svg className={styles.icon} fill="#fff" height="25px" width="25px" version="1.1" id="XMLID_268_" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" xmlSpace="preserve" stroke="none">
-    <g id="SVGRepo_bgCarrier" stroke-width="0" />
+    <g id="SVGRepo_bgCarrier" strokeWidth="0" />
     <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
     <g id="SVGRepo_iconCarrier">
       <g id="shop-basket">
