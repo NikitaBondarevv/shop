260ba2bbb88833c8ce797dd621144f7f11ccc8d7
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2023-02-09 03:12:15 +0200

    Remake hamburgerManu

--- src/components/header/hamburgerMenu/index.tsx
+++ src/components/header/hamburgerMenu/index.tsx
@@ -9,7 +9,14 @@ import { notLoggedNavigation } from 'helpers/notLoggedNavigation'
 export const HamburgerMenu = () => {
   const [toggleDisplayMenu, setToggleDisplayMenu] = useState('none')
   const hamburgerMenuRef = useRef<HTMLUListElement>(null)
+  const divFirstHamburger = useRef<HTMLDivElement>(null)
+  const divTwoHamburger = useRef<HTMLDivElement>(null)
+  const divLastHamburger = useRef<HTMLDivElement>(null)
   const { isAuthenticated } = useContext(UserContext)
+  const [opacityDivHamburger, setOpacityDivHamburger] = useState('')
+  const [rotateFirstDivHamburger, setRotateFirstDivHamburger] = useState('')
+  const [rotateLastDivHamburger, setRotateLastDivHamburger] = useState('')
+
 
   useEffect(() => window.addEventListener('resize', () => setToggleDisplayMenu('none')), [])
 
@@ -17,11 +24,27 @@ export const HamburgerMenu = () => {
     hamburgerMenuRef.current?.style.display === 'none'
       ? setToggleDisplayMenu('block')
       : setToggleDisplayMenu('none')
+
+    divFirstHamburger.current?.style.opacity === ''
+      ? setOpacityDivHamburger('0')
+      : setOpacityDivHamburger('')
+
+    divTwoHamburger.current?.style.transform === ''
+      ? setRotateFirstDivHamburger('45deg')
+      : setRotateFirstDivHamburger('')
+
+    divLastHamburger.current?.style.transform === ''
+      ? setRotateLastDivHamburger('-45deg')
+      : setRotateLastDivHamburger('')
   }
 
   return (
     <>
-      <div className={styles.hamburger} onClick={toggle}></div>
+      <button className={styles.hamburger} onClick={toggle}>
+        <div ref={divFirstHamburger} style={{ transform: divFirstHamburger.current?.style.transform === '' ? `rotate(${rotateFirstDivHamburger})` : '' }} />
+        <div ref={divTwoHamburger} style={{ opacity: divTwoHamburger.current?.style.opacity === '' ? opacityDivHamburger : '' }} />
+        <div ref={divLastHamburger} style={{ transform: divLastHamburger.current?.style.transform === '' ? `rotate(${rotateLastDivHamburger})` : '' }} />
+      </button>
       {
         isAuthenticated
           ? (
