8e0901ad694680e51c9bc2b62c63b048f5f3c3b9
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2023-02-16 22:30:52 +0200

    Move category and product to separate folders

--- src/components/productItems/index.tsx
+++ src/components/productItems/index.tsx
@@ -18,13 +18,13 @@ export const ProductItems = ({
   return (
     <div>
       <span className={styles.title}>
-        TITLE: <EditableText onBlur={(title) => setTitleProductProduct(title)} text={titleProduct} />
+        TITLE: <EditableText onBlur={(title) => setTitleProductProduct(title)} text={title} />
       </span>
       <span className={styles.price}>
-        $ <EditableText onBlur={(price) => setPriceProduct(price)} text={String(priceProduct)} price />
+        $ <EditableText onBlur={(price) => setPriceProduct(price)} text={String(price)} price />
       </span>
       <div className={styles.description}>
-        <EditableText classNameSpan={styles.descriptionText} onBlur={(description) => setDescriptionProduct(description)} text={descriptionProduct || 'Put your description here'} multiLine />
+        <EditableText classNameSpan={styles.descriptionText} onBlur={(description) => setDescriptionProduct(description)} text={descriptionProduct === '' ? 'Put your description here' : description} multiLine />
       </div>
       <Link to={`/products/${titleProduct}`} className={styles.save} onClick={() => onSave(titleProduct!, priceProduct, descriptionProduct!)}>SAVE</Link>
     </div>
