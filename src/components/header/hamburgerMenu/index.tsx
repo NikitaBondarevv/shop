3a717082888fe315f8bc6dd130aab8b2743491f7
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2023-02-21 04:35:55 +0200

    Code refactoring

--- src/components/header/hamburgerMenu/index.tsx
+++ src/components/header/hamburgerMenu/index.tsx
@@ -1,12 +1,22 @@
-import { useEffect, useRef, useState } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { navigation } from 'helpers/navigation'
 import styles from './styles.css'
+import { UserContext } from 'contexts/userContext'
+import { notLoggedNavigation } from 'helpers/notLoggedNavigation'
 
 export const HamburgerMenu = () => {
   const [toggleDisplayMenu, setToggleDisplayMenu] = useState('none')
   const hamburgerMenuRef = useRef<HTMLUListElement>(null)
+  const divFirstHamburger = useRef<HTMLDivElement>(null)
+  const divTwoHamburger = useRef<HTMLDivElement>(null)
+  const divLastHamburger = useRef<HTMLDivElement>(null)
+  const { isAuthenticated } = useContext(UserContext)
+  const [opacityDivHamburger, setOpacityDivHamburger] = useState('')
+  const [rotateFirstDivHamburger, setRotateFirstDivHamburger] = useState('')
+  const [rotateLastDivHamburger, setRotateLastDivHamburger] = useState('')
+
 
   useEffect(() => window.addEventListener('resize', () => setToggleDisplayMenu('none')), [])
 
@@ -14,20 +24,52 @@ export const HamburgerMenu = () => {
     hamburgerMenuRef.current?.style.display === 'none'
       ? setToggleDisplayMenu('block')
       : setToggleDisplayMenu('none')
+
+    divFirstHamburger.current?.style.opacity === ''
+      ? setOpacityDivHamburger('0')
+      : setOpacityDivHamburger('')
+
+    divTwoHamburger.current?.style.transform === ''
+      ? setRotateFirstDivHamburger('45deg')
+      : setRotateFirstDivHamburger('')
+
+    divLastHamburger.current?.style.transform === ''
+      ? setRotateLastDivHamburger('-45deg')
+      : setRotateLastDivHamburger('')
   }
 
   return (
     <>
-      <div className={styles.hamburger} onClick={toggle}></div>
-      <ul ref={hamburgerMenuRef} className={styles.hamburgerMenu} style={{ display: toggleDisplayMenu }}>
-        {navigation.map((link, index) => (
-          <li key={index}>
-            <Link to={`/${link.value}`} className={styles[link.value]}>
-              {link.text}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <button className={styles.hamburger} onClick={toggle}>
+        <div ref={divFirstHamburger} style={{ transform: divFirstHamburger.current?.style.transform === '' ? `rotate(${rotateFirstDivHamburger})` : '' }} />
+        <div ref={divTwoHamburger} style={{ opacity: divTwoHamburger.current?.style.opacity === '' ? opacityDivHamburger : '' }} />
+        <div ref={divLastHamburger} style={{ transform: divLastHamburger.current?.style.transform === '' ? `rotate(${rotateLastDivHamburger})` : '' }} />
+      </button>
+      {
+        isAuthenticated
+          ? (
+            <ul ref={hamburgerMenuRef} className={styles.hamburgerMenu} style={{ display: toggleDisplayMenu }}>
+              {navigation.map((link, index) => (
+                <li key={index}>
+                  <Link to={`/${link.value}`} className={styles[link.value]} onClick={() => setToggleDisplayMenu('none')}>
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )
+          : (
+            <ul ref={hamburgerMenuRef} className={styles.hamburgerMenu} style={{ display: toggleDisplayMenu }}>
+              {notLoggedNavigation.map((link, index) => (
+                <li key={index}>
+                  <Link to={`/${link.value}`} className={styles[link.value]} onClick={() => setToggleDisplayMenu('none')}>
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )
+      }
     </>
   )
 }
